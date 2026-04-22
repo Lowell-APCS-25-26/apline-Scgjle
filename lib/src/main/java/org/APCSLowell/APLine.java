91265ca8ee453a1e33907cbd0fef66e7bee2716e
Scgjle <ianplymale@gmail.com>
2026-04-22 16:52:44 +0000

    My dog ate my homework

--- lib/src/main/java/org/APCSLowell/APLine.java
+++ lib/src/main/java/org/APCSLowell/APLine.java
@@ -2,9 +2,13 @@ package org.APCSLowell;
 
 public class APLine {
     int a, b, c;
-    public APLine(int a, int b, int c) {}
+    public APLine(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
     public boolean isOnLine(int x, int y) {
-        return a * x + b * y + c == 0;
+        return (double)a * (double)x + (double)b * (double)y + (double)c == 0.0;
     }
     public double getSlope() {
         return -(double)a / (double)b;
